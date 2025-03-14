4d6352519f723b5f1f2a59c6adb039f1d6e8fb0b
revanadha <revansa888@gmail.com>
2025-03-14 20:40:02 +0800

    update first

--- Codelab/Modul-2/Codelab-1/src/hewan.java
+++ Codelab/Modul-2/Codelab-1/src/hewan.java
@@ -1,2 +1,8 @@
 public class hewan {
+    String Nama;
+    String Jenis;
+    String Suara;
+    void tampilkanInfo(){
+        System.out.println("\nNama  : " + Nama + "\nJenis : " + Jenis + "\nSuara : " + Suara);
+    }
 }
